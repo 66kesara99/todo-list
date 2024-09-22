8863ef59a8cfdb037d4e03959503e83518659216
Kesara Gamlath <66kesara99@gmail.com>
2024-09-22 12:06:52 +0530

    gh pages config change

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/todo-list/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
